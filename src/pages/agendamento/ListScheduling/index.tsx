90d3a02b0bbbf2912169df6c5dbcc38c5050e3c7
gdpnei2002 <joseneilson25@gmail.com>
2023-08-21 14:18:31 -0300

    agrupamento por semana

--- src/pages/agendamento/ListScheduling/index.tsx
+++ src/pages/agendamento/ListScheduling/index.tsx
@@ -1,8 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+interface Appointment {
+  id: string;
+  fields: {
+    clientName: string;
+    date: string;
+    time: string;
+  };
+}
+
 const AppointmentList: React.FC = () => {
-  const [appointments, setAppointments] = useState([]);
+  const [appointments, setAppointments] = useState<Appointment[]>([]);
+
+  const getStartOfWeek = (dateString: string) => {
+    const date = new Date(dateString);
+    const day = date.getDay();
+    const diff = date.getDate() - day;
+    const startOfWeek = new Date(date.setDate(diff)).toISOString().split("T")[0];
+    return startOfWeek;
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,17 +46,32 @@ const AppointmentList: React.FC = () => {
     fetchData();
   }, []);
 
+  const groupedAppointments: { [weekStart: string]: Appointment[] } = {};
+
+  appointments.forEach((appointment) => {
+    const weekStart = getStartOfWeek(appointment.fields.date);
+    if (!groupedAppointments[weekStart]) {
+      groupedAppointments[weekStart] = [];
+    }
+    groupedAppointments[weekStart].push(appointment);
+  });
+
   return (
     <div>
-      <h2>Lista de Agendamentos Ordenados por Horário</h2>
-      <ul>
-        {appointments.map((appointment: any) => (
-          <li key={appointment.id}>
-            {appointment.fields.clientName} - {appointment.fields.date}{" "}
-            {appointment.fields.time}
-          </li>
-        ))}
-      </ul>
+      <h2>Lista de Agendamentos Agrupados por Semana</h2>
+      {Object.entries(groupedAppointments).map(([weekStart, appointments]) => (
+        <div key={weekStart}>
+          <h3>Semana de {weekStart}</h3>
+          <ul>
+            {appointments.map((appointment) => (
+              <li key={appointment.id}>
+                {appointment.fields.clientName} - {appointment.fields.date}{" "}
+                {appointment.fields.time}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
